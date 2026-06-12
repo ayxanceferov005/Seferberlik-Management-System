d43873d53be2808d53f06682ac2c3079a79a6eab
Aykhan Jafarov <ayxan.ceferov.005@gmail.com>
2026-06-12 10:05:02 +0400

    CallUpGenerator faylında 2 field əlavə edildi

--- cagirislar/CallUpGenerator.java
+++ cagirislar/CallUpGenerator.java
@@ -4,5 +4,7 @@ public class CallUpGenerator {
         System.out.println("Generating mobilization call-up...");
         System.out.println("Citizen: " + citizenName);
         System.out.println("Status: CALL-UP GENERATED 📩");
+        System.out.println("Status: CALL-UP CREATED 📩")
+        System.out.println("Status: CALL-UP SENDED 📩")
     }
 }
